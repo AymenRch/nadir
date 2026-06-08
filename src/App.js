2b833a00e17cfde2739eea716f215e1643c59117
AymenRch <aymenrch37@gmail.com>
2026-06-09 00:11:31 +0200

    Clean up App.js by removing unused routes
    Removed commented-out routes from the App component.

--- src/App.js
+++ src/App.js
@@ -7,24 +7,12 @@ import CourtMetrageRoles from './screens/courtMetrageRoles';
 import PricingPage from './screens/PricingPage';
 import ArtistPage from './screens/ArtistPage';
 import RealisateurStudio from './screens/RealisateurStudio ';
+import ExpiredPage from './screens/expired';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-{
-  /*
-        <Route path="/RegisterWork" element={<RegisterWork/>} />
-      <Route path="/Payment" element={<PaymentPage />} />
-      <Route path="/Admin" element={<AdminDashboard />} />
-      <Route path="/court-metrage" >
-        <Route path="roles" element={<CourtMetrageRoles />} />
-        <Route path="pricing" element={<PricingPage />} />
-        <Route path="artist" element={<ArtistPage />} />
-        <Route path="studio" element={<RealisateurStudio />} />
-      </Route>
-  */
-}
     </Routes>
   )
 }

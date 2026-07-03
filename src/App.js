8db0523132ac71e0ce1e3e7fc1b1dfbba060ed8c
AymenRch <aymenrch37@gmail.com>
2026-07-03 22:32:54 +0200

    Uncomment RegisterWork and RealisateurStudio routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './screens/Home'
-{/*import RegisterWork from './screens/RegisterWork'
+import RegisterWork from './screens/RegisterWork'
 import PaymentPage from './screens/Payment'
 import AdminDashboard from './screens/dash';
 import CourtMetrageRoles from './screens/courtMetrageRoles';
 import PricingPage from './screens/PricingPage';
 import ArtistPage from './screens/ArtistPage';
-import RealisateurStudio from './screens/RealisateurStudio ';*/}
+import RealisateurStudio from './screens/RealisateurStudio ';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-{/*<Route path="/RegisterWork" element={<RegisterWork/>} />
+<Route path="/RegisterWork" element={<RegisterWork/>} />
       <Route path="/Payment" element={<PaymentPage />} />
       <Route path="/Admin" element={<AdminDashboard />} />
       <Route path="/court-metrage" >
@@ -20,7 +20,7 @@ function App() {
         <Route path="pricing" element={<PricingPage />} />
         <Route path="artist" element={<ArtistPage />} />
         <Route path="studio" element={<RealisateurStudio />} />
-      </Route>*/}
+      </Route>
     </Routes>
   )
 }

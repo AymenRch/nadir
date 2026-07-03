4939baf046cb53a17cfdba10d4fed86d6ec1070f
AymenRch <aymenrch37@gmail.com>
2026-07-03 22:24:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './screens/Home'
-import RegisterWork from './screens/RegisterWork'
+{/*import RegisterWork from './screens/RegisterWork'
 import PaymentPage from './screens/Payment'
 import AdminDashboard from './screens/dash';
 import CourtMetrageRoles from './screens/courtMetrageRoles';
 import PricingPage from './screens/PricingPage';
 import ArtistPage from './screens/ArtistPage';
-import RealisateurStudio from './screens/RealisateurStudio ';
+import RealisateurStudio from './screens/RealisateurStudio ';*/}
 
 function App() {
   return (
-     <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "2rem",
-        fontWeight: "bold",
-        textAlign: "center",
-      }}
-    >
-      You have to pay.
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+{/*<Route path="/RegisterWork" element={<RegisterWork/>} />
+      <Route path="/Payment" element={<PaymentPage />} />
+      <Route path="/Admin" element={<AdminDashboard />} />
+      <Route path="/court-metrage" >
+        <Route path="roles" element={<CourtMetrageRoles />} />
+        <Route path="pricing" element={<PricingPage />} />
+        <Route path="artist" element={<ArtistPage />} />
+        <Route path="studio" element={<RealisateurStudio />} />
+      </Route>*/}
+    </Routes>
   )
 }
 
